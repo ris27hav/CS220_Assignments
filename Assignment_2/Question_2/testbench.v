ac01227d1e6dbc65efedca685f0f8a586bc8d28b
Ujwal Jyot Panda <ujwal12366@gmail.com>
2022-02-03 22:58:52 +0530

    Add Question 2
    8-bit Johnson counter code done, need to chek if the testbench satisfies the conditions given in question

---
+++ Assignment_2/Question_2/testbench.v
@@ -0,0 +1,18 @@
+`include "johnson.v"
+
+module tb();
+    reg clk, rst;
+    wire [7:0] out;
+
+    johnson_counter test(clk,rst,out);
+
+    initial clk=0;
+    always #5 clk=~clk;
+
+    initial begin
+      $monitor("time = %0d, clk = %b, rst = %b, output = %b", $time, clk, rst, out);
+      rst=1;
+      #10 rst=0;
+      #155 $finish; 
+    end
+endmodule
