9369c62683cb622598f3c29d970144d0c213959b
AdiPratapSingh <apsinghnsa@gmail.com>
2022-02-14 20:22:24 +0530

    Question 1 done

--- Assignment_3/Question_1/FSM.v
+++ Assignment_3/Question_1/FSM.v
@@ -74,33 +74,48 @@ parameter s1 = 3'b000 , s2 = 3'b001, s3 = 3'b010, s4 = 3'b011, s5 = 3'b100;
 reg [2:0] state;
 
 always @* begin
-    state = sin;
-    b = 8'b00000000;
+    assign state = sin;
+    assign b = 8'b00000000;
     case(state)
         s1 : begin 
             if(a[7:4] == 4'b1010)begin
                 assign b = 8'b00010000;
             end
+            // else begin
+            //     assign b = 8'b00000000;
+            // end
         end
         s2 : begin 
             if(a[6:3] == 4'b1010)begin
                 assign b = 8'b00001000;
             end
+            // else begin
+            //     assign b = 8'b00000000;
+            // end
         end
         s3 : begin 
             if(a[5:2] == 4'b1010)begin
                 assign b = 8'b00000100;
             end
+            // else begin
+            //     assign b = 8'b00000000;
+            // end
         end
         s4 : begin 
             if(a[4:1] == 4'b1010)begin
                 assign b = 8'b00000010;
             end
+            // else begin
+            //     assign b = 8'b00000000;
+            // end
         end
         s5 : begin 
             if(a[3:0] == 4'b1010)begin
                 assign b = 8'b00000001;
             end
+            // else begin
+            //     assign b = 8'b00000000;
+            // end
         end
         default : assign b = 0;
     endcase
