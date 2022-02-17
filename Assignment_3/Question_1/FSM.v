0476b6a4cd4613876f5871eb8e822385c84b33a1
AdiPratapSingh <apsinghnsa@gmail.com>
2022-02-17 23:24:59 +0530

    All Done

--- Assignment_3/Question_1/FSM.v
+++ Assignment_3/Question_1/FSM.v
@@ -1,85 +1,23 @@
-// module fsm_sub(a,b,ans,clock);
-
-// input [7:0] a;
-
-// reg [7:0] ans;
-// reg [2:0] next_state;  // 0,1,2,3,4
-// reg [2:0] state;  // 0,1,2,3,4
-
-// output[7:0] b;
-// parameter s1 = 3'b000 , s2 = 3'b001, s3 = 3'b010, s4 = 3'b011, s5 = 3'b100;
-
-// // statewise output
-// always @ (state) begin
-//     case(state)
-//         s1 : begin 
-//             next_state <= s2;
-//             if(a[7:4] == 4'b1010)begin
-//                 ans <= ans||8'b00010000;
-//             end
-//             else begin 
-//             end
-//         end
-//         s2 : begin 
-//             next_state <= s3;
-//             if(a[7:4] == 4'b1010)begin
-//                 ans <= ans||8'b00001000;
-//             end
-//             else begin 
-//             end
-//         end
-//         s3 : begin 
-//             next_state <= s4;
-//             if(a[7:4] == 4'b1010)begin
-//                 ans <= ans||8'b00000100;
-//             end
-//             else begin 
-//             end
-//         end
-//         s4 : begin 
-//             next_state <= s5;
-//             if(a[7:4] == 4'b1010)begin
-//                 ans <= ans||8'b00000010;
-//             end
-//             else begin 
-//             end
-//         end
-//         s5 : begin 
-//             next_state <= 3'b111;
-//             if(a[7:4] == 4'b1010)begin
-//                 ans <= ans||8'b00000001;
-//             end
-//             else begin 
-//             end
-//         end
-//         default : ;
-//     endcase
-// end
-
-// always @ (posedge clock) begin
-//     state = next_state;
-//     clock = ~clock;
-// end
-
-// endmodule
-
-module fsm_sub(a,b,sin);
+module fsm_sub(a,b,sin,next_state);
 
 input [7:0] a;
 input [2:0] sin;
 
 output reg [7:0] b;
+output [2:0]next_state;
 
 parameter s1 = 3'b000 , s2 = 3'b001, s3 = 3'b010, s4 = 3'b011, s5 = 3'b100;
 reg [2:0] state;
 
+assign next_state = sin + 3'b001;
+
 always @* begin
-    assign state = sin;
-    assign b = 8'b00000000;
+    state = sin;
+    b = 8'b00000000;
     case(state)
         s1 : begin 
             if(a[7:4] == 4'b1010)begin
-                assign b = 8'b00010000;
+                b = 8'b00010000;
             end
             // else begin
             //     assign b = 8'b00000000;
@@ -87,7 +25,7 @@ always @* begin
         end
         s2 : begin 
             if(a[6:3] == 4'b1010)begin
-                assign b = 8'b00001000;
+                b = 8'b00001000;
             end
             // else begin
             //     assign b = 8'b00000000;
@@ -95,7 +33,7 @@ always @* begin
         end
         s3 : begin 
             if(a[5:2] == 4'b1010)begin
-                assign b = 8'b00000100;
+                b = 8'b00000100;
             end
             // else begin
             //     assign b = 8'b00000000;
@@ -103,7 +41,7 @@ always @* begin
         end
         s4 : begin 
             if(a[4:1] == 4'b1010)begin
-                assign b = 8'b00000010;
+                b = 8'b00000010;
             end
             // else begin
             //     assign b = 8'b00000000;
@@ -111,13 +49,13 @@ always @* begin
         end
         s5 : begin 
             if(a[3:0] == 4'b1010)begin
-                assign b = 8'b00000001;
+                b = 8'b00000001;
             end
             // else begin
             //     assign b = 8'b00000000;
             // end
         end
-        default : assign b = 0;
+        default :  b = 0;
     endcase
 end
 
--- Assignment_3/Question_1/FSM_driver.v
+++ Assignment_3/Question_1/FSM_driver.v
@@ -5,20 +5,24 @@ input [7:0]a;
 output [7:0]c;
 
 wire [7:0]b1;
-fsm_sub d1(a,b1,3'b000);
-
 wire [7:0]b2;
-fsm_sub d2(a,b2,3'b001);
-
 wire [7:0]b3;
-fsm_sub d3(a,b3,3'b010);
-
 wire [7:0]b4;
-fsm_sub d4(a,b4,3'b011);
-
 wire [7:0]b5;
-fsm_sub d5(a,b5,3'b100);
 
+wire [14:0]temp;
+
+fsm_sub d1(a,b1,3'b000,temp[2:0]);
+
+fsm_sub d2(a,b2,temp[2:0],temp[5:3]);
+
+fsm_sub d3(a,b3,temp[5:3],temp[8:6]);
+
+fsm_sub d4(a,b4,temp[8:6],temp[11:9]);
+
+fsm_sub d5(a,b5,temp[11:9],temp[14:12]);
+
+// taking bitwise or
 assign c = b1|b2|b3|b4|b5;
 
 endmodule
